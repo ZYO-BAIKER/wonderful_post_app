f67db096772055a604523a5f4c53a35aa3f67281
ZYO-BAIKER <keisukeminakawa@gmail.com>
2022-01-23 14:47:54 +0900

    記事とタグのアソシエーション

--- app/models/article.rb
+++ app/models/article.rb
@@ -1,4 +1,6 @@
 class Article < ApplicationRecord
+  has_many :tag_articles, dependent: :destroy
+  has_many :tags, through: :tag_articles
   belongs_to :user
 
   validates :title, :content, presence: true
